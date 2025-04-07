fb2663c6b6241a1d91814c58c3325ec8af8dd4db
lhachete <mhmonllor@hotmail.com>
2025-04-07 17:27:38 +0200

    Rutas, MatDrawer, MatToolbar y mas importaciones de Angular Material

--- sistemas-pagos-frontend/src/app/app-routing.module.ts
+++ sistemas-pagos-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { PagosComponent } from './pagos/pagos.component';
+import { EstudiantesComponent } from './estudiantes/estudiantes.component';
+import { LoadEstudiantesComponent } from './load-estudiantes/load-estudiantes.component';
+import { LoadPagosComponent } from './load-pagos/load-pagos.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: "",component:LoginComponent},
+  {path: "home",component:HomeComponent},
+  {path: "profile",component:ProfileComponent},
+  {path: "login",component:HomeComponent},
+  {path: "pagos",component:PagosComponent},
+  {path: "estudiantes",component:EstudiantesComponent},
+  {path: "loadEstudiantes",component:LoadEstudiantesComponent},
+  {path: "loadPagos",component:LoadPagosComponent},
+  {path: "dashboard",component:DashboardComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- sistemas-pagos-frontend/src/app/app.module.ts
+++ sistemas-pagos-frontend/src/app/app.module.ts
@@ -4,12 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminTemplateComponent } from './admin-template/admin-template.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatListModule} from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
 import { LoadEstudiantesComponent } from './load-estudiantes/load-estudiantes.component';
@@ -18,6 +18,7 @@ import { LoginComponent } from './login/login.component';
 import { PagosComponent } from './pagos/pagos.component';
 import { EstudiantesComponent } from './estudiantes/estudiantes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     LoginComponent,
     PagosComponent,
     EstudiantesComponent,
-    DashboardComponent
+    DashboardComponent,
+
   ],
   imports: [
     BrowserModule,
@@ -40,7 +42,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     MatIconModule,
     MatMenuModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
