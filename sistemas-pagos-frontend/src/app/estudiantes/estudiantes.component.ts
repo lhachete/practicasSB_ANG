e93e6b1f12f1b7d8257236feeb6145d426541362
lhachete <mhmonllor@hotmail.com>
2025-04-08 15:16:09 +0200

    Tabla para filtrar estudiantes

--- sistemas-pagos-frontend/src/app/estudiantes/estudiantes.component.ts
+++ sistemas-pagos-frontend/src/app/estudiantes/estudiantes.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Estudiante } from '../models/estudiantes.model';
+import { EstudiantesService } from '../services/estudiantes.service';
 
 @Component({
   selector: 'app-estudiantes',
@@ -6,6 +8,21 @@ import { Component } from '@angular/core';
   templateUrl: './estudiantes.component.html',
   styleUrl: './estudiantes.component.css'
 })
-export class EstudiantesComponent {
+export class EstudiantesComponent implements OnInit{
+
+  estudiantes!: Array<Estudiante>;
+
+  constructor(private estudianteService:EstudiantesService){}
+
+  ngOnInit(): void {
+    this.estudianteService.getAllEstudiantes().subscribe({
+      next: value => {
+        this.estudiantes = value;
+      },
+      error: err => {
+        console.log(err);
+      }
+    })
+  }
 
 }
--- sistemas-pagos-frontend/src/app/pagos/pagos.component.ts
+++ sistemas-pagos-frontend/src/app/pagos/pagos.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { EstudiantesService } from '../services/estudiantes.service';
 
 @Component({
   selector: 'app-pagos',
@@ -20,9 +21,9 @@ export class PagosComponent implements OnInit {
   @ViewChild(MatPaginator)paginator!: MatPaginator;
   @ViewChild (MatSort) sort!: MatSort;
 
-  constructor(private http:HttpClient){}
+  constructor(private http:HttpClient, private estudianteService: EstudiantesService){}
   ngOnInit(): void {
-      this.http.get("http://localhost:8080/pagos").subscribe({
+      this.estudianteService.getAllPagos().subscribe({
         next: data => {
           this.pagos = data;
           this.dataSource = new MatTableDataSource(this.pagos);
